de4df09b5844989538f67b497b7b8a0edcdf9289
Vojtěch Struhár <vojtechstruhar@gmail.com>
2022-10-07 13:56:30 +0200

    Added shadows, fixed spawner bug.

--- Minigames/Tyrian/TyrianEnemySpawner.gd
+++ Minigames/Tyrian/TyrianEnemySpawner.gd
@@ -6,7 +6,7 @@ onready var lowerBound = $"../LowerBound"
 onready var enemyTemplate = preload("res://Minigames/Tyrian/TyrianEnemy.tscn")
 
 func _on_Timer_timeout():
-	var random_y = rand_range(upperBound.position.y + 20, lowerBound.position.y - 20)
+	var random_y = rand_range(upperBound.global_position.y + 20, lowerBound.global_position.y - 20)
 	var enemy = enemyTemplate.instance()
 	add_child(enemy)
-	enemy.position.y = random_y
+	enemy.global_position.y = random_y
