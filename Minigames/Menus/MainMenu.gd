b593f235fa59d5a645c866980840d0aec1ea4aa2
Vojtěch Struhár <vojtechstruhar@gmail.com>
2022-10-10 21:32:40 +0200

    Main menu UI reworked.
    * Improved main menu layout.
    * Set rules for the window itself, so it keeps aspect ratio and can be maximized reasonably.
    + Added "Tyrian" icon
    
    Main menu adjusted margins

--- Minigames/Menus/MainMenu.gd
+++ Minigames/Menus/MainMenu.gd
@@ -3,6 +3,5 @@ extends Control
 onready var fletnicka = $MicrophoneInput
 
 func _process(_delta: float):
-	$"%NoteLabel".text = NoteFrequencies.names[round(fletnicka.current_note)]
-	$"%PercentageSign".text = str(round(fletnicka.current_note_normalized * 100) / 100.0)
-	
+	$"%NoteLabel".text = NoteFrequencies.names[round(fletnicka.current_note)].split(" ")[0]
+
