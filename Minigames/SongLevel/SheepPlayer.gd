e84c6ae5b3340b726104356f078126e58fc4ee84
Vojtěch Struhár <vojtechstruhar@gmail.com>
2022-11-01 19:22:04 +0100

    Added loot and tree prefabs.
    + Added animated tree - random sway speed.
    + Added money bag as a loot

--- Minigames/SongLevel/SheepPlayer.gd
+++ Minigames/SongLevel/SheepPlayer.gd
@@ -6,12 +6,20 @@ onready var fletnicka = $"../MicrophoneInput"
 
 export var MOVEMENT_SPEED = 100
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var extents = rightBound.position.x - leftBound.position.x
 	
 	var target_x = fletnicka.current_note_normalized * extents + leftBound.position.x
+	var diff = (target_x - position.x)
+	# This slows down the sheep, caps the amount of movement it does
+	var final_diff = min(abs(diff), MOVEMENT_SPEED) * sign(diff)
+	
+	if abs(final_diff) < 0.01: final_diff = 0
+	
+	if final_diff < 0: $Sheep.scale.x = -1.1
+	elif final_diff > 0: $Sheep.scale.x = 1.1
 	
 	var tween := create_tween().set_trans(Tween.TRANS_LINEAR)
-	tween.tween_property(self, "position:x", target_x, 0.1)
-	
+	# warning-ignore:return_value_discarded
+	tween.tween_property(self, "position:x", position.x + final_diff, 0.1)
 	
--- Minigames/SongLevel/SongMinigame.gd
+++ Minigames/SongLevel/SongMinigame.gd
@@ -1,7 +1,31 @@
 extends Node2D
 
-var song_resource: SongResource = null
+var song_resource: SongResource = preload("res://Assets/Songs/bday_100bpm.tres")
 
+func _ready():
+	$StartGameTimer.start()
 
 func start_game():
 	if song_resource == null: return
+	
+	print("GAME STARTED")
+	$ObstacleSpawner/BpmTimer.wait_time = 60 / song_resource.bpm
+	$ObstacleSpawner/BpmTimer.start()
+	
+	$Grass.speed = song_resource.bpm
+	$Grass.is_running = true
+	
+	$SheepPlayer/Sheep.playing = true
+
+func finish_game():
+	pass
+
+
+func _on_StartGameTimer_timeout():
+	start_game()
+
+
+
+func _on_MoneyScanner_area_entered(area):
+	area.queue_free()
+	print("+1")
