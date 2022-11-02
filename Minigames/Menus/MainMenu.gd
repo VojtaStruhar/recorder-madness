4a7fee9ae082f6fc5a72097f732c6c33c379004c
Vojtěch Struhár <vojtechstruhar@gmail.com>
2022-11-02 10:12:01 +0100

    Fixed obstacles appearing on screen.
    + Added quit game button to main menu
    
    Added pressed texture (I think it doesn't work anyways)

--- Minigames/Menus/MainMenu.gd
+++ Minigames/Menus/MainMenu.gd
@@ -5,3 +5,7 @@ onready var fletnicka = $MicrophoneInput
 func _process(_delta: float):
 	$"%NoteLabel".text = "-" if fletnicka.is_quiet else NoteFrequencies.names[round(fletnicka.current_note)].split(" ")[0]	
 	$"%ErrorLabel".text = fletnicka.error_message
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
